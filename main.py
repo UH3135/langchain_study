7771feac0f01ee8d87271ab565a78a262e24108b
UH3135 <rabbitdy0101@kookmin.ac.kr>
2024-11-03 19:36:39 +0900

    FEAT: add chat mode

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
+from pydantic import BaseModel, Field
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from typing import List, Union
+
 
 app = FastAPI()
 app.add_middleware(
@@ -17,8 +21,25 @@ app.add_middleware(
 
 @app.get("/")
 async def redirect_root_to_docs():
-    return RedirectResponse("/chat/playground")
-add_routes(app, chain, path="/chat")
+    return RedirectResponse("/prompt/playground")
+add_routes(app, chain, path="/prompt")
+
+class InputChat(BaseModel):
+    """Input for the chat endpoint."""
+
+    messages: List[Union[HumanMessage, AIMessage, SystemMessage]] = Field(
+        ...,
+        description="The chat messages representing the current conversation.",
+    )
+
+add_routes(
+    app,
+    chain.with_types(input_type=InputChat),
+    path="/chat",
+    enable_feedback_endpoint=True,
+    enable_public_trace_link_endpoint=True,
+    playground_type="chat",
+)
 
 logging.langsmith("llm_study")
 
@@ -26,5 +47,5 @@ if __name__ == "__main__":
     import uvicorn
     
     uvicorn.run(app, host="localhost", port=8000)
-    # http://localhost:8000/chat/playground/
+    # http://localhost:8000/prompt/playground/
     # 위 링크에 접속
